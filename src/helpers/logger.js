52da210285afc5c1e9e96be56559343f8a3bc15d
Beniston Jeyapaul <beniston.j@listertechnologies.com>
2016-08-26 20:39:18 +0530

    last_changes_after_arranges

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,4 +1,4 @@
 'use strict'
-var log={}
+var log = {}
 log.info = console.log
 module.exports = log
--- src/services/inventory_logging_service.js
+++ src/services/inventory_logging_service.js
@@ -4,16 +4,16 @@ var objPost = new postgre()
 
 module.exports = function inventory_logging_service(options) {
 
-	this.add({role: 'inventory_logging', cmd: 'log_product_availability'}, function (msg, respond) {
+	this.add({ role: 'inventory_logging', cmd: 'log_product_availability' }, function (msg, respond) {
 		logger.info("Received event for logging product availability")
- 		var obj= {};
+		var obj = {};
 		obj.eventName = 'AddItem'
-	    obj.productId = 21309
-	    obj.sessionId = '12345'
+		obj.productId = 21309
+		obj.sessionId = '12345'
 		obj.jsonstring = '"warehouse":"nc","quantity":10'
-		
-		objPost.logInventoryDetails.call(objPost,obj)
-	    var response = {success:true}
-	    respond(null, response)
-  	});
+
+		objPost.logInventoryDetails.call(objPost, obj)
+		var response = { success: true }
+		respond(null, response)
+	});
 }
